ce8de69b0fa6ab21fc5553ede8f3738d2d65df91
Ilia_Stavrov <petromarz@yandex.ru>
2024-11-22 14:21:29 +0100

    window to follow

--- src/main/java/org/home/stavrov/ImitationOfIntenseActivity.java
+++ src/main/java/org/home/stavrov/ImitationOfIntenseActivity.java
@@ -4,7 +4,7 @@ import org.home.stavrov.windows.MainWindow;
 
 public class ImitationOfIntenseActivity {
 
-    public static final int DELAY = 2000;
+    public static final int DELAY = 10000;
 
     public static void main(String[] args) {
         var window = new MainWindow();
